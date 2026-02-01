bd780b08beca185cfa4875d1afdaa00f92799e2d
l3erkeo <berkeozer@protonmail.com>
2026-02-02 00:48:49 +0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ben,-seviyor-musun-/",
+  base: "/beni-seviyor-musun-/",
   plugins: [react()],
 });
